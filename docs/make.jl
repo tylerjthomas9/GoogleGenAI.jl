cad6610994e5e4add57d3c7606c5214bdbac5ca2
Tyler Thomas <tylerjthomas9@gmail.com>
2024-02-11 00:17:39 -0700

    add docs

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using GoogleGenAI
+using Documenter
+
+DocMeta.setdocmeta!(GoogleGenAI, :DocTestSetup, :(using GoogleGenAI); recursive=true)
+
+makedocs(;
+    modules=[GoogleGenAI],
+    authors="Tyler Thomas <tylerjthomas9@gmail.com>",
+    repo="https://github.com/tylerjthomas9/GoogleGenAI.jl.git",
+    sitename="GoogleGenAI.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://tylerjthomas9.github.io/GoogleGenAI.jl",
+        assets=String[],
+    ),
+    pages=["Home" => "index.md"],
+    warnonly=true,
+)
+
+deploydocs(; repo="github.com/tylerjthomas9/GoogleGenAI.jl.git")
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,9 +1,4 @@
- [![Aqua QA](https://raw.githubusercontent.com/JuliaTesting/Aqua.jl/master/badge.svg)](https://github.com/JuliaTesting/Aqua.jl)
- [![Code Style: Blue](https://img.shields.io/badge/code%20style-blue-4495d1.svg)](https://github.com/invenia/BlueStyle)
- [![Docs](https://img.shields.io/badge/docs-dev-blue.svg)](https://tylerjthomas9.github.io/GoogleGenAI.jl)
-
-
-# GoogleGenAI.jl
+# GoogleGenAI.jl Docs
 
 ## Overview
 
